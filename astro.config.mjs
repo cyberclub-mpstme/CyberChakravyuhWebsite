db22eb0a5c3ad4fe6b1690982a676530ed2435f7
Yuvraj Bishnoi <yuvi.bishnoi@gmail.com>
2026-07-30 02:23:27 +0530

    fix: update vercel adapter import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
