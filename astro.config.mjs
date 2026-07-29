82c3961111395b6d4a29199be5669b8ebbe216af
Yuvraj Bishnoi <yuvi.bishnoi@gmail.com>
2026-07-30 02:20:53 +0530

    fix: added vercel adapter for keystatic SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
-  output: 'static'
+  output: 'hybrid',
+  adapter: vercel(),
 });
