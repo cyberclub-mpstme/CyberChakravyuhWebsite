f8b665ef62484be0b9d9b186e57355e44bfbeb52
Yuvraj Bishnoi <yuvi.bishnoi@gmail.com>
2026-08-04 01:07:13 +0530

    fix: add site to astro config to fix vercel SSR routing for keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cyber-chakravyuh-website.vercel.app',
   integrations: [react(), markdoc(), keystatic()],
   output: 'server',
   adapter: vercel(),
